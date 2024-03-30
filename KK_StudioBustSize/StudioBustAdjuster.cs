e1030797af2914b01a41440dc5bbacc4816df110
Vaaskr <38697288+Vaaskr@users.noreply.github.com>
2024-03-30 17:31:40 -0500

    Add files via upload

---
+++ KK_StudioBustSize/StudioBustAdjuster.cs
@@ -0,0 +1,199 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using KK_Plugins;
+using KKABMX.Core;
+using KKAPI;
+using KKAPI.Chara;
+using KKAPI.Studio;
+using KKAPI.Studio.UI;
+using MessagePack;
+using Studio;
+using System;
+using UniRx;
+using UnityEngine;
+using static Illusion.Utils;
+
+namespace KK_StudioBustAdjuster
+{
+    [BepInPlugin(GUID, "Studio Bust Adjuster", Version)]
+    [BepInDependency(KoikatuAPI.GUID, KoikatuAPI.VersionConst)]
+    [BepInDependency(KKABMX_Core.GUID, "5.1")]
+    [BepInDependency(KK_Plugins.Pushup.GUID, "1.3.2")]
+    public partial class StudioBustAdjuster : BaseUnityPlugin
+    {
+        public const string GUID = "Vaaskr.StudioBustAdjuster";
+        public const string Version = "1.0.0";
+        internal static new ManualLogSource Logger;
+
+        private void Awake()
+        {
+            if (StudioAPI.InsideStudio)
+            {
+                StudioBustAdjuster.Logger = base.Logger;
+            }
+        }
+        private void Start()
+        {
+            if (StudioAPI.InsideStudio)
+            {
+                this.RegisterStudioControls();
+                StudioBustSizeTimeline.PopulateTimeline();
+                CharacterApi.RegisterExtraBehaviour<StudioBustAdjusterController>(GUID);
+            }
+        }
+        private void RegisterStudioControls()
+        {
+            CurrentStateCategory BustStateCategories = StudioAPI.GetOrCreateCurrentStateCategory("Studio Bust Adjuster");
+            ObservableExtensions.Subscribe<float>(BustStateCategories.AddControl<CurrentStateCategorySlider>(new CurrentStateCategorySlider("   Size", (OCIChar Char) => StudioObjectExtensions.GetChaControl(Char).GetComponent<StudioBustAdjusterController>().BustSize, 0f, 1f)).Value, delegate (float f)
+            {
+                StudioBustAdjuster.UpdateBustAdjustmentProportions(f, StudioBustAdjuster.Bust.Size);
+            });
+            ObservableExtensions.Subscribe<float>(BustStateCategories.AddControl<CurrentStateCategorySlider>(new CurrentStateCategorySlider("   Vertical Position", (OCIChar Char) => StudioObjectExtensions.GetChaControl(Char).GetComponent<StudioBustAdjusterController>().BustVertPos, -1f, 2f)).Value, delegate (float f)
+            {
+                StudioBustAdjuster.UpdateBustAdjustmentProportions(f, StudioBustAdjuster.Bust.VertPos);
+            });
+            ObservableExtensions.Subscribe<float>(BustStateCategories.AddControl<CurrentStateCategorySlider>(new CurrentStateCategorySlider("   Horizontal Angle", (OCIChar Char) => StudioObjectExtensions.GetChaControl(Char).GetComponent<StudioBustAdjusterController>().BustHorzAngle, -1f, 2f)).Value, delegate (float f)
+            {
+                StudioBustAdjuster.UpdateBustAdjustmentProportions(f, StudioBustAdjuster.Bust.HorzAngle);
+            });
+            ObservableExtensions.Subscribe<float>(BustStateCategories.AddControl<CurrentStateCategorySlider>(new CurrentStateCategorySlider("   Horizontal Position", (OCIChar Char) => StudioObjectExtensions.GetChaControl(Char).GetComponent<StudioBustAdjusterController>().BustHorzPos, -1f, 2f)).Value, delegate (float f)
+            {
+                StudioBustAdjuster.UpdateBustAdjustmentProportions(f, StudioBustAdjuster.Bust.HorzPos);
+            });
+            ObservableExtensions.Subscribe<float>(BustStateCategories.AddControl<CurrentStateCategorySlider>(new CurrentStateCategorySlider("   Vertical Angle", (OCIChar Char) => StudioObjectExtensions.GetChaControl(Char).GetComponent<StudioBustAdjusterController>().BustVertAngle, -1f, 2f)).Value, delegate (float f)
+            {
+                StudioBustAdjuster.UpdateBustAdjustmentProportions(f, StudioBustAdjuster.Bust.VertAngle);
+            });
+            ObservableExtensions.Subscribe<float>(BustStateCategories.AddControl<CurrentStateCategorySlider>(new CurrentStateCategorySlider("   Pointiness", (OCIChar Char) => StudioObjectExtensions.GetChaControl(Char).GetComponent<StudioBustAdjusterController>().BustPointness, -1f, 2f)).Value, delegate (float f)
+            {
+                StudioBustAdjuster.UpdateBustAdjustmentProportions(f, StudioBustAdjuster.Bust.Pointiness);
+            });
+            ObservableExtensions.Subscribe<float>(BustStateCategories.AddControl<CurrentStateCategorySlider>(new CurrentStateCategorySlider("   Shape", (OCIChar Char) => StudioObjectExtensions.GetChaControl(Char).GetComponent<StudioBustAdjusterController>().BustShape, -1f, 2f)).Value, delegate (float f)
+            {
+                StudioBustAdjuster.UpdateBustAdjustmentProportions(f, StudioBustAdjuster.Bust.Shape);
+            });
+            ObservableExtensions.Subscribe<float>(BustStateCategories.AddControl<CurrentStateCategorySlider>(new CurrentStateCategorySlider("   Softness", (OCIChar Char) => StudioObjectExtensions.GetChaControl(Char).GetComponent<StudioBustAdjusterController>().BustSoftness, -1f, 2f)).Value, delegate (float f)
+            {
+                StudioBustAdjuster.UpdateBustAdjustmentProportions(f, StudioBustAdjuster.Bust.Softness);
+            });
+            ObservableExtensions.Subscribe<float>(BustStateCategories.AddControl<CurrentStateCategorySlider>(new CurrentStateCategorySlider("   Weight", (OCIChar Char) => StudioObjectExtensions.GetChaControl(Char).GetComponent<StudioBustAdjusterController>().BustWeight, -1f, 2f)).Value, delegate (float f)
+            {
+                StudioBustAdjuster.UpdateBustAdjustmentProportions(f, StudioBustAdjuster.Bust.Weight);
+            });
+            ObservableExtensions.Subscribe<float>(BustStateCategories.AddControl<CurrentStateCategorySlider>(new CurrentStateCategorySlider("   Areola Bulge", (OCIChar Char) => StudioObjectExtensions.GetChaControl(Char).GetComponent<StudioBustAdjusterController>().AreolaBulge, -1f, 2f)).Value, delegate (float f)
+            {
+                StudioBustAdjuster.UpdateBustAdjustmentProportions(f, StudioBustAdjuster.Bust.AreolaBulge);
+            });
+            ObservableExtensions.Subscribe<float>(BustStateCategories.AddControl<CurrentStateCategorySlider>(new CurrentStateCategorySlider("   Nipple Circumference", (OCIChar Char) => StudioObjectExtensions.GetChaControl(Char).GetComponent<StudioBustAdjusterController>().NipCircumfrence, -1f, 2f)).Value, delegate (float f)
+            {
+                StudioBustAdjuster.UpdateBustAdjustmentProportions(f, StudioBustAdjuster.Bust.NipCircum);
+            });
+            ObservableExtensions.Subscribe<float>(BustStateCategories.AddControl<CurrentStateCategorySlider>(new CurrentStateCategorySlider("   Nipple Protrusion", (OCIChar Char) => StudioObjectExtensions.GetChaControl(Char).GetComponent<StudioBustAdjusterController>().NipProtrusion, -1f, 2f)).Value, delegate (float f)
+            {
+                StudioBustAdjuster.UpdateBustAdjustmentProportions(f, StudioBustAdjuster.Bust.NipProtrusion);
+            });
+        }
+        internal static void UpdateBustAdjustmentProportions(float value, StudioBustAdjuster.Bust bust)
+        {
+            foreach (OCIChar ociChar in StudioAPI.GetSelectedCharacters())
+            {
+                StudioBustAdjuster.UpdateBustAdjustmentProportions(ociChar, value, bust);
+            }
+        }
+        internal static void UpdateBustAdjustmentProportions(OCIChar ociChar, float value, StudioBustAdjuster.Bust bust)
+        {
+            ChaControl chaControl = StudioObjectExtensions.GetChaControl(ociChar);
+            StudioBustAdjusterController bustController = chaControl.GetComponent<StudioBustAdjusterController>();
+
+            if (bust == StudioBustAdjuster.Bust.Size)
+            {
+                bustController.BustSize = value;
+                bustController.setPushupBodyData();
+                return;
+            }
+            if (bust == StudioBustAdjuster.Bust.VertPos)
+            {
+                bustController.BustVertPos = value;
+                bustController.setPushupBodyData();
+                return;
+            }
+            if (bust == StudioBustAdjuster.Bust.HorzAngle)
+            {
+                bustController.BustHorzAngle = value;
+                bustController.setPushupBodyData();
+                return;
+            }
+            if (bust == StudioBustAdjuster.Bust.HorzPos)
+            {
+                bustController.BustHorzPos = value;
+                bustController.setPushupBodyData();
+                return;
+            }
+            if (bust == StudioBustAdjuster.Bust.VertAngle)
+            {
+                bustController.BustVertAngle = value;
+                bustController.setPushupBodyData();
+                return;
+            }
+            if (bust == StudioBustAdjuster.Bust.Pointiness)
+            {
+                bustController.BustPointness= value;
+                bustController.setPushupBodyData();
+                return;
+            }
+            if (bust == StudioBustAdjuster.Bust.Shape)
+            {
+                bustController.BustShape = value;
+                bustController.setPushupBodyData();
+                return;
+            }
+            if (bust == StudioBustAdjuster.Bust.Softness)
+            {
+                bustController.BustSoftness = value;
+                bustController.setPushupBodyData();
+                return;
+            }
+            if (bust == StudioBustAdjuster.Bust.Weight)
+            {
+                bustController.BustWeight = value;
+                bustController.setPushupBodyData();
+                return;
+            }
+            if (bust == StudioBustAdjuster.Bust.AreolaBulge)
+            {
+                bustController.AreolaBulge = value;
+                bustController.setPushupBodyData();
+                return;
+            }
+            if (bust == StudioBustAdjuster.Bust.NipCircum)
+            {
+                bustController.NipCircumfrence = value;
+                bustController.setPushupBodyData();
+                return;
+            }
+            if (bust == StudioBustAdjuster.Bust.NipProtrusion)
+            {
+                bustController.NipProtrusion = value;
+                bustController.setPushupBodyData();
+                return;
+            }
+        }
+        internal enum Bust
+        {
+            Null,
+            Size,
+            VertPos,
+            HorzAngle,
+            HorzPos,
+            VertAngle,
+            Pointiness,
+            Shape,
+            Softness,
+            Weight,
+            AreolaBulge,
+            NipCircum,
+            NipProtrusion,
+            AreolaSize,
+        }
+    }
+}
---
+++ KK_StudioBustSize/StudioBustAdjusterController.cs
@@ -0,0 +1,392 @@
+﻿using ExtensibleSaveFormat;
+using Illusion.Component.Correct;
+using KK_Plugins;
+using KKABMX.Core;
+using KKAPI;
+using KKAPI.Chara;
+using KKAPI.Studio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using static Illusion.Utils;
+using static KK_Plugins.Pushup;
+
+namespace KK_StudioBustAdjuster
+{
+    internal class StudioBustAdjusterController : CharaCustomFunctionController
+    {
+
+        public float BustSize
+        {
+            get
+            {
+                return ChaControl.GetComponent<Pushup.PushupController>().BaseData.Size;
+            }
+            set
+            {;
+                ChaControl.GetComponent<Pushup.PushupController>().BaseData.Size = value;
+            }
+        }
+        public float BustVertPos
+        {
+            get
+            {
+                return ChaControl.GetComponent<Pushup.PushupController>().BaseData.VerticalPosition;
+            }
+            set
+            {
+                ChaControl.GetComponent<Pushup.PushupController>().BaseData.VerticalPosition = value;
+            }
+        }
+        public float BustHorzAngle
+        {
+            get
+            {
+                return ChaControl.GetComponent<Pushup.PushupController>().BaseData.HorizontalAngle;
+            }
+            set
+            {
+                ChaControl.GetComponent<Pushup.PushupController>().BaseData.HorizontalAngle = value;
+            }
+        }
+        public float BustHorzPos
+        {
+            get
+            {
+                return ChaControl.GetComponent<Pushup.PushupController>().BaseData.HorizontalPosition;
+            }
+            set
+            {
+                ChaControl.GetComponent<Pushup.PushupController>().BaseData.HorizontalPosition = value;       
+            }
+        }
+        public float BustVertAngle
+        {
+            get
+            {
+                return ChaControl.GetComponent<Pushup.PushupController>().BaseData.VerticalAngle;
+            }
+            set
+            {
+                ChaControl.GetComponent<Pushup.PushupController>().BaseData.VerticalAngle = value;
+            }
+        }
+        public float BustPointness
+        {
+            get
+            {
+                return ChaControl.GetComponent<Pushup.PushupController>().BaseData.Depth;
+            }
+            set
+            {
+                ChaControl.GetComponent<Pushup.PushupController>().BaseData.Depth = value;
+            }
+        }
+        public float BustShape
+        {
+            get
+            {
+                return ChaControl.GetComponent<Pushup.PushupController>().BaseData.Roundness;
+            }
+            set
+            {
+                ChaControl.GetComponent<Pushup.PushupController>().BaseData.Roundness = value;
+            }
+        }
+        public float BustSoftness
+        {
+            get
+            {
+                return ChaControl.GetComponent<Pushup.PushupController>().BaseData.Softness;
+            }
+            set
+            {
+                ChaControl.GetComponent<Pushup.PushupController>().BaseData.Softness = value;
+            }
+        }
+        public float BustWeight
+        {
+            get
+            {
+                return ChaControl.GetComponent<Pushup.PushupController>().BaseData.Weight;
+            }
+            set
+            {
+                ChaControl.GetComponent<Pushup.PushupController>().BaseData.Weight = value;
+            }
+        }
+        public float AreolaBulge
+        {
+            get
+            {
+                return ChaControl.GetComponent<Pushup.PushupController>().BaseData.AreolaDepth;
+            }
+            set
+            {
+                ChaControl.GetComponent<Pushup.PushupController>().BaseData.AreolaDepth = value;
+            }
+        }
+        public float NipCircumfrence
+        {
+            get
+            {
+                return ChaControl.GetComponent<Pushup.PushupController>().BaseData.NippleWidth;
+            }
+            set
+            {
+                ChaControl.GetComponent<Pushup.PushupController>().BaseData.NippleWidth = value;
+            }
+        }
+        public float NipProtrusion
+        {
+            get
+            {
+                return ChaControl.GetComponent<Pushup.PushupController>().BaseData.NippleDepth;
+            }
+            set
+            {
+                ChaControl.GetComponent<Pushup.PushupController>().BaseData.NippleDepth = value;
+            }
+        }
+        protected override void OnCardBeingSaved(GameMode currentGameMode)
+        {
+            PluginData pluginData = new PluginData();
+            pluginData.data["BustSize"] = this.BustSize;
+            pluginData.data["BustVerticalPosition"] = this.BustVertPos;
+            pluginData.data["BustHorizontalAngle"] = this.BustHorzAngle;
+            pluginData.data["BustHorizontalPosition"] = this.BustHorzPos;
+            pluginData.data["BustVerticalAngle"] = this.BustVertAngle;
+            pluginData.data["BustPointness"] = this.BustPointness;
+            pluginData.data["BustShape"] = this.BustShape;
+            pluginData.data["AreolaBulge"] = this.AreolaBulge;
+            pluginData.data["NippleCircumfrence"] = this.NipCircumfrence;
+            pluginData.data["NippleProtrusion"] = this.NipProtrusion;
+            base.SetExtendedData(pluginData);
+        }
+        protected override void OnReload(GameMode currentGameMode)
+        {
+            PluginData extendedData = base.GetExtendedData();
+            if (extendedData != null)
+            {
+                object obj;
+                if (extendedData.data.TryGetValue("BustSize", out obj))
+                {
+                    this.BustSize = (float)obj;
+                }
+                object obj2;
+                if (extendedData.data.TryGetValue("BustVerticalPosition", out obj2))
+                {
+                    this.BustVertPos = (float)obj2;
+                }
+                object obj3;
+                if (extendedData.data.TryGetValue("BustHorizontalAngle", out obj3))
+                {
+                    this.BustHorzAngle = (float)obj3;
+                }
+                object obj4;
+                if (extendedData.data.TryGetValue("BustHorizontalPosition", out obj4))
+                {
+                    this.BustHorzPos = (float)obj4;
+                }
+                object obj5;
+                if (extendedData.data.TryGetValue("BustVerticalAngle", out obj5))
+                {
+                    this.BustVertAngle = (float)obj5;
+                }
+                object obj6;
+                if (extendedData.data.TryGetValue("BustPointness", out obj6))
+                {
+                    this.BustPointness = (float)obj6;
+                }
+                object obj7;
+                if (extendedData.data.TryGetValue("BustShape", out obj7))
+                {
+                    this.BustShape = (float)obj7;
+                }
+                object obj8;
+                if (extendedData.data.TryGetValue("AreolaBulge", out obj8))
+                {
+                    this.AreolaBulge = (float)obj8;
+                }
+                object obj9;
+                if (extendedData.data.TryGetValue("NippleCircumfrence", out obj9))
+                {
+                    this.NipCircumfrence = (float)obj9;
+                }
+                object obj10;
+                if (extendedData.data.TryGetValue("NippleProtrusion", out obj10))
+                {
+                    this.NipProtrusion = (float)obj10;
+                }
+            }
+        }
+        public void setPushupBodyData()
+        {
+            Wearing nowWearing = PushupCurrentlyWearing;
+            if (nowWearing == Wearing.None)
+            {
+                RemapPushUpDataToChaFile(ChaControl.GetComponent<Pushup.PushupController>().BaseData);
+            }
+            else
+            {
+                RecalculatePush(nowWearing);
+                RemapPushUpDataToChaFile(ChaControl.GetComponent<Pushup.PushupController>().CurrentPushupData);
+            }
+        }
+        internal void RemapPushUpDataToChaFile(BodyData bodyData)
+        {
+            void setShapeValue(int idx, float val)
+            {
+                ChaControl.fileBody.shapeValueBody[idx] = val;
+                ChaControl.SetShapeBodyValue(idx, val);
+            }
+
+            ChaControl.ChangeBustSoftness(bodyData.Softness);
+            ChaControl.ChangeBustGravity(bodyData.Weight);
+
+            setShapeValue(4, bodyData.Size);
+            setShapeValue(5, bodyData.VerticalPosition);
+            setShapeValue(6, bodyData.HorizontalAngle);
+            setShapeValue(7, bodyData.HorizontalPosition);
+            setShapeValue(8, bodyData.VerticalAngle);
+            setShapeValue(9, bodyData.Depth);
+            setShapeValue(10, bodyData.Roundness);
+            setShapeValue(11, bodyData.AreolaDepth);
+            setShapeValue(12, bodyData.NippleWidth);
+            setShapeValue(13, bodyData.NippleDepth);
+        }
+        internal void RecalculatePush(Wearing wearing)
+        {
+            PushupController pushupController = ChaControl.GetComponent<Pushup.PushupController>();
+            if (wearing == Wearing.Bra)
+            {
+                RecalculatePushFromClothes(pushupController.CurrentBraData, pushupController.CurrentBraData.UseAdvanced);
+                return;
+            }
+            if (wearing == Wearing.Top)
+            {
+                RecalculatePushFromClothes(pushupController.CurrentTopData, pushupController.CurrentTopData.UseAdvanced);
+                return;
+            }
+            if (pushupController.CurrentTopData.UseAdvanced)
+            {
+                RecalculatePushFromClothes(pushupController.CurrentTopData, true);
+                return;
+            }
+            if (pushupController.CurrentBraData.UseAdvanced)
+            {
+                RecalculatePushFromClothes(pushupController.CurrentBraData, true);
+                return;
+            }
+
+            var combo = new ClothData();
+            combo.Firmness = System.Math.Max(pushupController.CurrentBraData.Firmness, pushupController.CurrentTopData.Firmness);
+            combo.Lift = System.Math.Max(pushupController.CurrentBraData.Lift, pushupController.CurrentTopData.Lift);
+            combo.Squeeze = System.Math.Max(pushupController.CurrentBraData.Squeeze, pushupController.CurrentTopData.Squeeze);
+            combo.PushTogether = System.Math.Max(pushupController.CurrentBraData.PushTogether, pushupController.CurrentTopData.PushTogether);
+            combo.CenterNipples = System.Math.Max(pushupController.CurrentBraData.CenterNipples, pushupController.CurrentTopData.CenterNipples);
+            combo.FlattenNipples = pushupController.CurrentBraData.FlattenNipples || pushupController.CurrentTopData.FlattenNipples;
+            combo.EnablePushup = true;
+
+            RecalculatePushFromClothes(combo, false);
+        }
+
+        internal void RecalculatePushFromClothes(ClothData cData, bool useAdvanced)
+        {
+            PushupController pushupController = ChaControl.GetComponent<Pushup.PushupController>();
+            if (useAdvanced)
+            {
+                cData.CopyTo(pushupController.CurrentPushupData);
+                return;
+            }
+
+            if (1f - cData.Firmness < pushupController.BaseData.Softness)
+            {
+                pushupController.CurrentPushupData.Softness = 1 - cData.Firmness;
+            }
+            else
+            {
+                pushupController.CurrentPushupData.Softness = pushupController.BaseData.Softness;
+            }
+
+            if (cData.Lift > pushupController.BaseData.VerticalPosition)
+            {
+                pushupController.CurrentPushupData.VerticalPosition = cData.Lift;
+            }
+            else
+            {
+                pushupController.CurrentPushupData.VerticalPosition = pushupController.BaseData.VerticalPosition;
+            }
+
+            if (1f - cData.PushTogether < pushupController.BaseData.HorizontalAngle)
+            {
+                pushupController.CurrentPushupData.HorizontalAngle = 1 - cData.PushTogether;
+            }
+            else
+            {
+                pushupController.CurrentPushupData.HorizontalAngle = pushupController.BaseData.HorizontalAngle;
+            }
+
+            if (1f - cData.PushTogether < pushupController.BaseData.HorizontalPosition)
+            {
+                pushupController.CurrentPushupData.HorizontalPosition = 1 - cData.PushTogether;
+            }
+            else
+            {
+                pushupController.CurrentPushupData.HorizontalPosition = pushupController.BaseData.HorizontalPosition;
+            }
+
+            if (1f - cData.Squeeze < pushupController.BaseData.Depth)
+            {
+                pushupController.CurrentPushupData.Depth = 1 - cData.Squeeze;
+                pushupController.CurrentPushupData.Size = pushupController.BaseData.Size + (pushupController.BaseData.Depth - (1 - cData.Squeeze)) / 10;
+            }
+            else
+            {
+                pushupController.CurrentPushupData.Depth = pushupController.BaseData.Depth;
+                pushupController.CurrentPushupData.Size = pushupController.BaseData.Size;
+            }
+
+            if (cData.FlattenNipples)
+            {
+                pushupController.CurrentPushupData.NippleDepth = 0f;
+                pushupController.CurrentPushupData.AreolaDepth = 0f;
+            }
+            else
+            {
+                pushupController.CurrentPushupData.NippleDepth = pushupController.BaseData.NippleDepth;
+                pushupController.CurrentPushupData.AreolaDepth = pushupController.BaseData.AreolaDepth;
+            }
+
+            pushupController.CurrentPushupData.NippleWidth = pushupController.BaseData.NippleWidth;
+
+            var nipDeviation = 0.5f - pushupController.BaseData.VerticalAngle;
+            pushupController.CurrentPushupData.VerticalAngle = 0.5f - (nipDeviation - (nipDeviation * cData.CenterNipples));
+
+            pushupController.CurrentPushupData.Weight = pushupController.BaseData.Weight;
+            pushupController.CurrentPushupData.Roundness = pushupController.BaseData.Roundness;
+        }
+        private Wearing PushupCurrentlyWearing
+        {
+            get
+            {
+                var braIsOnAndEnabled = PushupEnabledAndBraOn;
+                var topIsOnAndEnabled = PushupEnabledAndTopOn;
+
+                if (topIsOnAndEnabled)
+                    return braIsOnAndEnabled ? Wearing.Both : Wearing.Top;
+
+                return braIsOnAndEnabled ? Wearing.Bra : Wearing.None;
+            }
+        }
+        public bool PushupEnabledAndBraOn => ChaControl.IsClothesStateKind((int)ChaFileDefine.ClothesKind.bra) &&
+                                              ChaControl.fileStatus.clothesState[(int)ChaFileDefine.ClothesKind.bra] == 0 &&
+                                              ChaControl.GetComponent<Pushup.PushupController>().CurrentBraData.EnablePushup;
+        public bool PushupEnabledAndTopOn => ChaControl.IsClothesStateKind((int)ChaFileDefine.ClothesKind.top) &&
+                                              ChaControl.fileStatus.clothesState[(int)ChaFileDefine.ClothesKind.top] == 0 &&
+                                              ChaControl.GetComponent<Pushup.PushupController>().CurrentTopData.EnablePushup;
+        internal enum Wearing { None, Bra, Top, Both }
+    }
+}
